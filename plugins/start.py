c024a0e75dab9ca0cff50866f3136bd209db6b7b
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-17 16:23:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -53,7 +53,7 @@ async def help_me(bot, message):
 @Client.on_message(filters.command("help"))
 async def help(bot, message):
   await bot.send_sticker(message.chat.id, S_STICKER)
-  await message.reply_message(text=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
+  await message.reply_text(text=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
 
     
     
