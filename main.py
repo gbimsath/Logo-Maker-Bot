f744fe602c1b19d97bd65f60222e080f67101a1d
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-17 15:23:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client ,idle
+from config import *
+
+plugins = dict(root="plugins")
+
+bot=Client( 
+    "Image upload bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=plugins)
+
+bot.start()
+uname = (bot.get_me()).username
+a = f"""
+{uname} has been deployed!
+"""
+idle()
