dc00c36741e13d25fd41cda5d4413fe21446400f
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-17 16:03:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,6 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-START_BUTTON = InlineKeyboardMarkup([[              
-                 InlineKeyboardButton('• 𝙊𝙪𝙩𝙧𝙪𝙞𝙓 • ™', url=f"https://t.me/TeamOutruix")
-                 ],
-                 [
-                 InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴜʀ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/{}t?startgroup=true")
-                 ]]
-                  )
-
 START_STING = """
 **🔮 Hello There, You Can Use Me To Create Awesome Logos...**
 ➤ Click /help Or The Button Below To Know How To Use Me
