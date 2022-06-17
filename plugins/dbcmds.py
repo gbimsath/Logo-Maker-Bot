a20f167f2a7dafab734c7b5c1f822d4b059863cf
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-17 19:11:51 +0530

    Update dbcmds.py

--- plugins/dbcmds.py
+++ plugins/dbcmds.py
@@ -42,6 +42,22 @@ import datetime
 DATABASE_URL=MONGO_URI
 db = Database(DATABASE_URL, "LOGOMAKER")
 
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+
 @Client.on_message(filters.command("status"), group=5)
 async def status(bot, update):
     if not await db.is_user_exist(update.from_user.id):
