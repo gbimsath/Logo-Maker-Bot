12cfafad697c1a07cefeee5dc575261c26587da2
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-17 18:47:19 +0530

    Create dbcmds..py

---
+++ plugins/dbcmds..py
@@ -0,0 +1,113 @@
+from database.db import Database
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from config import *
+import logging
+from database.db import Database
+from asyncio import sleep
+import traceback
+from database.db import Database
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import *
+from pyrogram.errors import *
+from pyrogram.types import *
+from config import *
+import logging
+from database.db import Database
+from asyncio import sleep
+import traceback
+from helper.fsub import forcesub
+import re
+import uuid
+import socket
+import platform
+import os
+import random
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import pymongo
+import aiofiles
+import motor.motor_asyncio
+import datetime
+
+DATABASE_URL=MONGO_URI
+db = Database(DATABASE_URL, "LOGOMAKER")
+
+@Client.on_message(filters.user(OWNER_ID) & filters.command("status"), group=5)
+async def status(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+         await db.add_user(update.from_user.id)
+         
+    await bot.send_sticker(update.chat.id, random.choice(STAT_STICKER))
+    total_users = await db.total_users_count()
+    text = "**Bot Advanced Statistics 📊**\n"
+    text += f"\n**Total Users:** `{total_users}`"
+
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
+
+@Client.on_message(
+    filters.user(OWNER_ID) &
+    filters.private &
+    filters.command("broadcast") &
+    filters.reply
+)
+async def broadcast(bot, update, broadcast_ids={}):
+    all_users = await db.get_all_users()
+    broadcast_msg= update.reply_to_message
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+
+    out = await update.reply_text(text=f"Broadcast Started! You will be notified with log file when all the users are notified.")
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+        
+    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+        
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    
+    if failed == 0:
+        await update.reply_text(text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
+    else:
+        await update.reply_document(document='broadcast.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+      
+    os.remove('broadcast.txt')
